b2db3fd0fc2ce66939c4ca986fabaa1b456d9f80
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-06-01 12:39:31 +0530

    Added more linked list methods

--- src/DataStructures/SinglyLinkedList/singlyLinkedList.js
+++ src/DataStructures/SinglyLinkedList/singlyLinkedList.js
@@ -65,6 +65,63 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index > this.length) return null;
+    let current = this.head;
+    let count = 0;
+    while (count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+
+  set(value, index) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(value);
+    if (index === this.length) return !!this.push(value);
+
+    const newNode = new Node(value);
+    let prevNode = this.get(index - 1);
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let prevNode = this.get(index - 1);
+    const removedNode = prevNode.next;
+    prevNode.next = prevNode.next.next;
+    this.length--;
+    return removedNode;
+  }
+
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr, this.length);
+  }
+
+  reverse() {}
 }
 
 const obj = new SinglyLinkedList();
@@ -73,7 +130,10 @@ obj.push(85);
 obj.push(90);
 obj.pop();
 obj.pop();
-obj.pop();
 obj.unshift(75);
 obj.unshift(100);
-console.log(obj);
+obj.set(23, 0);
+obj.insert(99, 2);
+// console.log(obj);
+// console.log(obj.remove(1));
+obj.print();
