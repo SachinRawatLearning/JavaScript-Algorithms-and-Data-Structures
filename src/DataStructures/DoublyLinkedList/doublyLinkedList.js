ddd356db6031626f50a01d688e44bed5ae5db7e3
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-06-10 18:07:44 +0530

    Added Set, Insert, Remove methods for DLL

--- src/DataStructures/DoublyLinkedList/doublyLinkedList.js
+++ src/DataStructures/DoublyLinkedList/doublyLinkedList.js
@@ -98,6 +98,41 @@ class DoublyLinkedList {
     return foundNode;
   }
 
+  set(val, index) {
+    let foundNode = this.get(index);
+    foundNode && (foundNode.val = val);
+    return this;
+  }
+
+  insert(val, index) {
+    if (index < 0 || index > this.length) return null;
+    if (index === 0) return this.unshift(val);
+    if (index === this.length) return this.push(val);
+
+    let newNode = new Node(val);
+    let prevNode = this.get(index - 1);
+    prevNode.next.prev = newNode;
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
+    newNode.prev = prevNode;
+    this.length++;
+    return this;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let removedNode = this.get(index);
+    removedNode.prev.next = removedNode.next;
+    removedNode.next.prev = removedNode.prev;
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length--;
+    return removedNode;
+  }
+
   print() {
     let arr = [];
     let currNode = this.head;
@@ -118,4 +153,7 @@ first.pop();
 first.shift();
 first.unshift(44);
 console.log(first.get(2));
+first.set(33, 2);
+first.insert(2, 2);
+first.remove(2);
 first.print();
