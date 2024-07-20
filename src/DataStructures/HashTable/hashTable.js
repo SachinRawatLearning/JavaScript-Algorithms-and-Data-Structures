44304809d8877032b2083bd4f14ecffc0c638824
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-07-20 10:55:51 +0530

    HashTable get/set methods

--- src/DataStructures/HashTable/hashTable.js
+++ src/DataStructures/HashTable/hashTable.js
@@ -6,7 +6,7 @@ class HashTable {
   //Hash Function
   _hash(key) {
     let total = 0;
-    let prime = 31;
+    const prime = 31;
 
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       let value = key.charCodeAt(i) - 96;
@@ -17,12 +17,33 @@ class HashTable {
 
   _set(key, value) {
     const arrIndex = this._hash(key);
-    console.log(arrIndex);
-    //Separate Chaining
-    if (this.keyMap[arrIndex]) {
+
+    //Separate Chaining - store data at same spot using nested data structure
+    if (!this.keyMap[arrIndex]) this.keyMap[arrIndex] = [];
+    this.keyMap[arrIndex].push([key, value]);
+  }
+
+  _get(key) {
+    const arrIndex = this._hash(key);
+
+    if (!this.keyMap[arrIndex]) return undefined;
+
+    for (let i = 0; i < this.keyMap[arrIndex].length; i++) {
+      if (this.keyMap[arrIndex][i][0] === key)
+        return this.keyMap[arrIndex][i][1];
     }
   }
-}
 
-const ht = new HashTable();
-ht._set("pink");
+  _print() {
+    for (let ele of this.keyMap) console.log(ele);
+  }
+}
+const ht = new HashTable(17);
+ht._set("maroon", "#800000");
+ht._set("yellow", "#FFFF00");
+ht._set("olive", "#808000");
+ht._set("salmon", "#FA8072");
+ht._set("lightcoral", "#F08080");
+ht._set("mediumvioletred", "#C71585");
+ht._set("plum", "#DDA0DD");
+console.log(ht._get("yellow"));
