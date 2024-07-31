b804f62a902f2d33ffa9d7f52cb0852fbbf7e3cd
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-07-31 20:17:03 +0530

    Breadth First Interatively solution

---
+++ src/DataStructures/GraphTraversal/breadthFirstIteratively.js
@@ -0,0 +1,62 @@
+class Graph {
+  constructor() {
+    this._adjacencyList = {};
+  }
+
+  _addVertex(vertex) {
+    if (!this._adjacencyList[vertex]) this._adjacencyList[vertex] = [];
+  }
+
+  _addEdge(vertex1, vertex2) {
+    this._adjacencyList[vertex1].push(vertex2);
+    this._adjacencyList[vertex2].push(vertex1);
+  }
+
+  _bfs(startingVertex) {
+    const queue = [];
+    queue.push(startingVertex);
+    const visitedNodes = [];
+    const visitedObj = {};
+    visitedObj[startingVertex] = true;
+
+    while (queue.length) {
+      const vertex = queue.shift();
+      visitedNodes.push(vertex);
+      this._adjacencyList[vertex].forEach((vertex) => {
+        if (!visitedObj[vertex]) {
+          visitedObj[vertex] = true;
+          queue.push(vertex);
+        }
+      });
+    }
+    return visitedNodes;
+  }
+}
+
+const gh = new Graph();
+gh._addVertex("A");
+gh._addVertex("B");
+gh._addVertex("C");
+gh._addVertex("D");
+gh._addVertex("E");
+gh._addVertex("F");
+
+gh._addEdge("A", "B");
+gh._addEdge("A", "C");
+gh._addEdge("B", "D");
+gh._addEdge("C", "E");
+gh._addEdge("D", "E");
+gh._addEdge("D", "F");
+gh._addEdge("E", "F");
+
+console.log(gh._bfs("A")); //[ 'A', 'B', 'C', 'D', 'E', 'F' ]
+
+/*
+              A
+           /     \
+          B       C  
+          |       |  
+          D  ---  E
+          \      /
+              F
+      */
